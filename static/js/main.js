6fe5ef84d3a1bd5de680f2a761bfe5efa2e6126d
Kristy Stienstra <kristystienstra@gmail.com>
2018-02-21 06:59:19 -0800

    added results js and html for zillow api

--- static/js/main.js
+++ static/js/main.js
@@ -79,3 +79,23 @@ function initMap(){
 
 }
 
+//=========================Zillow API/Results Page============================================
+//vars
+var result = document.getElementById("zest");
+var address = "2114+Bigelow+Ave";
+var city = "Seattle";
+var state = "WA";
+
+var apiUrl = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz18s18qx40ln_4vl2o&address="+address+"&citystatezip="+city+"%2C+" + state;
+
+//display result
+function displayResult(res){
+	result.innerHTML = "$ " + res;
+}
+
+//fetch API without CORS should display as {object}
+var myInit = {mode : "no-cors"};
+fetch(apiUrl, myInit)
+	.then(response => response.text())
+	.then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    .then(data => displayResult(data));

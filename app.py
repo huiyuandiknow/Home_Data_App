bf91e53122bf4811e6f653ca8b60599c1a34964d
mikle <mihailgorbunov74@gmail.com>
2018-03-14 01:54:30 -0700

    Statistics were added on the about page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, redirect, url_for, session
+
+from cookies_session import ChunkedSecureCookieSessionInterface
 from data_handling import Results
 from flask_config import get_app
-from cookies_session import ChunkedSecureCookieSessionInterface
-from statistics_db_model import UserEnvironment, User, Environment, CheckPoint
 from statistics import Statistics
 
 app = get_app()
@@ -17,56 +17,7 @@ def make_session_permanent():
 # Index Page
 @app.route('/')
 def index():
-
-    #make_session_permanent()
-    #Statistics.handle_homepage('hp')
-    environment = UserEnvironment()
-    current_env = environment.get_current_user_environment()
-
-    try:
-        user_id = session['id']
-        u = Statistics.handle_page('hp', user_id, current_env)
-        if u:
-            session['id'] = u
-        #     session['id'] = 44
-
-        # user_id = session['id']
-        # print(user_id)
-        # user_id =user_id + 1
-        # session['id'] = user_id
-    except:
-        #Statistics.handle_page('hp', None, current_env)
-        session['id'] = Statistics.handle_page('hp', None, current_env)
-        #print(current_env.version)
-        #session['id'] = 95555899878
-
-    # try:
-    #     user_id = session['id']
-    #     existing_user = User.get_user_by_id(user_id)
-    #     if existing_user:
-    #         last_env = Environment.get_env_by_id(existing_user.last_env_id)
-    #         if last_env.is_equal(current_env):
-    #             check_point = CheckPoint(existing_user.last_env_id, "hp")
-    #             check_point.add_new()
-    #         else:
-    #             new_env = Environment(current_env, user_id)
-    #             new_env.add_new()
-    #             existing_user.change_last_env(new_env.id)
-    #             check_point = CheckPoint(existing_user.last_env_id, "hp")
-    #             check_point.add_new()
-    # except:
-    #     new_user = User()
-    #     new_user.add_new()
-    #     new_env = Environment(current_env, new_user.id)
-    #     new_env.add_new()
-    #     new_user.change_last_env(new_env.id)
-    #     session['id'] = new_user.id
-    #     check_point = CheckPoint(new_user.last_env_id, "hp")
-    #     check_point.add_new()
-
-
-    response = render_template('home.html')
-    return response
+    return Statistics.set_stats('hp', 'home.html')
 
 
 # Results Page
@@ -76,11 +27,13 @@ def show_results():
 
     zil = ""
     zil_home = ""
+
     try:
         address = request.form['address']
     except:
         return redirect(url_for('index'))
     # address = ''
+
     beds = request.form['beds'].replace('Bed: ', '')
     baths = request.form['baths'].replace('Bath: ', '')
     living = request.form['living']
@@ -110,7 +63,8 @@ def show_results():
 # About Page
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return Statistics.set_stats('ab', 'about.html')
+
 
 if __name__ == '__main__':
     app.run()
--- statistics.py
+++ statistics.py
@@ -1,8 +1,16 @@
-from flask_sqlalchemy import SQLAlchemy
+rom
+flask_sqlalchemy
+import SQLAlchemy
 from flask_config import get_app
 from statistics_db_model import UserEnvironment, User, Environment, CheckPoint
 #from coockies_session import ChunkedSecureCookieSessionInterface
-#from flask import session, render_template
+from flask import session, render_template
+# from coockies_session import ChunkedSecureCookieSessionInterface
+from flask import session, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_config import get_app
+from statistics_db_model import UserEnvironment, User, Environment, CheckPoint
 
 app = get_app()
 db = SQLAlchemy(app)
@@ -10,35 +18,6 @@ file = None
 #app.session_interface = ChunkedSecureCookieSessionInterface()
 
 class Statistics:
-    # @staticmethod
-    # def handle_homepage(page):
-    #
-    #     environment = UserEnvironment()
-    #     current_env = environment.get_current_user_environment()
-    #
-    #     try:
-    #         user_id = session['id']
-    #         existing_user = User.get_user_by_id(user_id)
-    #         if existing_user:
-    #             last_env = Environment.get_env_by_id(existing_user.last_env_id)
-    #             if last_env.is_equal(current_env):
-    #                 check_point = CheckPoint(existing_user.last_env_id, page)
-    #                 check_point.add_new()
-    #             else:
-    #                 new_env = Environment(current_env, user_id)
-    #                 new_env.add_new()
-    #                 existing_user.change_last_env(new_env.id)
-    #                 check_point = CheckPoint(existing_user.last_env_id, page)
-    #                 check_point.add_new()
-    #     except:
-    #         new_user = User()
-    #         new_user.add_new()
-    #         new_env = Environment(current_env, new_user.id)
-    #         new_env.add_new()
-    #         new_user.change_last_env(new_env.id)
-    #         session['id'] = new_user.id
-    #         check_point = CheckPoint(new_user.last_env_id, page)
-    #         check_point.add_new()
 
     @staticmethod
     def handle_page(page, user_id, current_env):
@@ -77,3 +56,17 @@ class Statistics:
             check_point.add_new()
             res = new_user.id
         return res
+
+    @staticmethod
+    def set_stats(page, template):
+        environment = UserEnvironment()
+        current_env = environment.get_current_user_environment()
+        try:
+            user_id = session['id']
+            u = Statistics.handle_page(page, user_id, current_env)
+            if u:
+                session['id'] = u
+        except:
+            session['id'] = Statistics.handle_page(page, None, current_env)
+        response = render_template(template)
+        return response
